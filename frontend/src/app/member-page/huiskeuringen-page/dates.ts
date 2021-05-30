b255c64c7d78af8078dcaffd9654f61c655046cd
Jan <Wazner@users.noreply.github.com>
2021-05-30 17:01:34 +0200

    Damn zero-based months... (#70)

--- frontend/src/app/member-page/huiskeuringen-page/dates.ts
+++ frontend/src/app/member-page/huiskeuringen-page/dates.ts
@@ -1,26 +1,26 @@
 export default [
     // noord
     [
-        new Date(2020, 6, 19),
-        new Date(2020, 6, 20),
-        new Date(2020, 6, 21),
-        new Date(2020, 8, 21),
-        new Date(2020, 8, 22),
-        new Date(2020, 8, 23)
+        new Date(2020, 5, 19),
+        new Date(2020, 5, 20),
+        new Date(2020, 5, 21),
+        new Date(2020, 7, 21),
+        new Date(2020, 7, 22),
+        new Date(2020, 7, 23)
     ],
     // oost
     [
-        new Date(2020, 6, 27),
-        new Date(2020, 7, 29)
+        new Date(2020, 5, 27),
+        new Date(2020, 6, 29)
     ],
     // west
     [
-        new Date(2020, 7, 5),
-        new Date(2020, 8, 1)
+        new Date(2020, 6, 5),
+        new Date(2020, 7, 1)
     ],
     // zuid
     [
-        new Date(2020, 7, 11),
-        new Date(2020, 8, 9)
+        new Date(2020, 6, 11),
+        new Date(2020, 7, 9)
     ]
 ];
