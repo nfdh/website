4b460a34223c003e93b947596ae551fb78823762
Jan <Wazner@users.noreply.github.com>
2021-12-14 00:59:19 +0100

    Fix api calls using wrong URL (#128)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nieuw.drentsheideschaap.nl',
+  apiUrl: 'https://drentsheideschaap.nl',
   appInsights: {
     instrumentationKey: '46da6ab2-ce36-48bf-9a04-5759a5f23d45'
   }
