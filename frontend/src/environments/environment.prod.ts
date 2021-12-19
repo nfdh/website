07885ad62e23a56ba21ff7dd8b7079b1a0fc1600
Jan <Wazner@users.noreply.github.com>
2021-12-19 20:38:03 +0100

    Fix api hostname (#130)
    * Fix postcss build error
    
    * Fix hostname not matching url in production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://drentsheideschaap.nl',
+  apiUrl: '',
   appInsights: {
     instrumentationKey: '46da6ab2-ce36-48bf-9a04-5759a5f23d45'
   }
